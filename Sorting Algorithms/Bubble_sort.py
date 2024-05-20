1a90ef144d596ec70410149031d412d3c5139c35
Usama Tahir <51717472+Usama00004@users.noreply.github.com>
2024-05-20 20:13:29 +0200

    Delete Sorting Algorithms directory

--- Sorting Algorithms/Bubble_sort.py
+++
@@ -1,9 +0,0 @@
-list_to_be_sorted=[2,8,6,7,5,9,8,3,7,8,6,]
-def bubble_sort(list):
-    for i in range(0,len(list)-1):
-        for j in range(0,len(list)-1-i):
-            if list[j] > list[j+1]:
-                list[j],list[j+1] = list[j+1],list[j]
-    return list
-sorted_list = bubble_sort(list_to_be_sorted)
-print(sorted_list)
--- Sorting Algorithms/Quick_sort.py
+++
@@ -1,5 +0,0 @@
-list_fruits = ['app','man','banana','Grape']
-
-for index,item in enumerate(list_fruits):
-    print(index)
-    print(item)
