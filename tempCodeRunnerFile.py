6bed6b7ea88e7730115cf8893326bc4d26078aba
Usama Tahir <usamatahir00004@gmail.com>
2024-10-29 14:02:11 +0100

    reading and implementing

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+# Object 
+
+temp = 98.5
+print(f"temp = {temp}")
---
+++ testing_concepts.py
@@ -0,0 +1,28 @@
+##### Object Referencing #####
+#creating oject reference with name temp
+temp = 98.5
+print(f"temp = {temp}")
+new_temp = temp
+print(f"new_temp = {new_temp}")
+print("============================")
+temp = temp + 5.0
+print(f"temp = {temp}")
+print(f"new_temp = {new_temp}")
+
+
+##### Default Syntax of bool #####
+# default syntax of bool return false
+print(bool())
+
+##### Default Syntax of int #####
+# default syntax of int returns 0
+print(int())
+print(int("3"))
+print(int(3.99))
+print(int(3.1))
+print(int(-3.99))
+
+##### Default Syntax of float #####
+print(float())
+print(float("3.14"))
+
