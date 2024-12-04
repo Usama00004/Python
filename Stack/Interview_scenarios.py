b356193597c438f69ab3cbdf1bc00ccb6bbd5a1a
Usama Tahir <usamatahir00004@gmail.com>
2024-12-04 01:58:37 +0100

    Interview Questions Stack

---
+++ Stack/Interview_scenarios.py
@@ -0,0 +1,6 @@
+# Question: Given a string containing parentheses, write a function to check if the parentheses are balanced
+
+# Question: Given an unsorted stack, sort the stack using only one additional stack. You cannot use any other data structures.
+
+
+# Question: Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
